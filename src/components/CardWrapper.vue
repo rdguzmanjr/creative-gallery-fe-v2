0ab879a2a4196342d8536a251bb106661c39e941
“rdguzmanjr” <jr.dguzman@gmail.com>
2024-04-16 21:23:55 +0800

    few adjus

--- src/components/CardWrapper.vue
+++ src/components/CardWrapper.vue
@@ -2,9 +2,9 @@
    
           <div id="card-wrapper" class="absolute w-screen pb-20 flex flex-col items-center gap-y-16
           md:grid md:grid-cols-[repeat(2,minmax(320px,450px))] md:justify-items-center
-          lg:right-0 lg:w-4/5
-          xl:grid-cols-[repeat(3,minmax(320px,430px))] 
-          2xl:justify-items-end">
+          lg:right-0 lg:w-4/5 lg:justify-center
+          xl:grid-cols-[repeat(3,minmax(300px,340px))] 
+          2xl:grid-cols-[repeat(3,minmax(300px,390px))] ">
                <slot></slot>
           </div>
  
