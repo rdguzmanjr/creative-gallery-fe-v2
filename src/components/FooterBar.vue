f4e05ad9db6aa40c054ff134ce8d4edfc5b8977e
rdguzmanjr <jr.dguzman@gmail.com>
2025-05-22 02:41:20 +0800

    footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="footer" class="bg-brandgray w-screen h-16 fixed bottom-0 border-t-[2px] border-[#00d592] hidden md:block z-10">
+    <div id="footer" class="bg-brandgray w-screen h-16 fixed bottom-0 border-t-[3px] border-[#00d592] hidden md:block z-10">
         <div id="footer-container" class="flex flex-row justify-between items-center h-16 mx-20">
            
             <div id="back" class="text-[#f5ffde] text-md space-x-4" @click="$router.go(-1)">
